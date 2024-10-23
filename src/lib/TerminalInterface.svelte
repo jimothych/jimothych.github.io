65538183a8ec2e6280867db533648f141e534ea9
James Chang <jameschang2005@icloud.com>
2024-10-23 15:37:39 -0400

    actually fixed issue this time

--- src/lib/TerminalInterface.svelte
+++ src/lib/TerminalInterface.svelte
@@ -67,7 +67,7 @@
   }
 
   async function handleAfterSubmitProcess() {
-    await sleep(50); //simulating latency, also conveniently pushes to bottom of div
+    await sleep(0); //conveniently pushes focus to bottom of div
     inputElementVisible = true;
     await sleep(0); //hack to bypass odd Svelte DOM rendering and get the input to focus again
     document.getElementById("mainInput").focus();
